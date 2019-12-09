0ba4bea8d25cff450e0bf120cd6ee28c341229fe
Anand Panangadan <apanangadan@fullerton.edu>
2019-12-08 22:37:21 -0800

    updated main.cpp to allow inexact max bucket size

--- main.cpp
+++ main.cpp
@@ -21,6 +21,9 @@ bool assertEquals(const string & nameOfTest, double expected, double actual);
 template <typename T, typename U>
 bool assertEquals(const string & nameOfTest, const T & expected, const U & actual);
 
+template <typename T, typename U>
+bool assertEquals(const string & nameOfTest, const T & expected, const U & actual, unsigned delta);
+
 // Main
 int main() {
 
@@ -30,7 +33,7 @@ int main() {
     assertEquals("wordCount: spot", 3, Spot.wordCount("spot"));
     assertEquals("wordCount: run", 4, Spot.wordCount("run"));
     assertEquals("mostFrequentWord: Spot", "run", Spot.mostFrequentWord());
-    assertEquals("maxBucketSize: Spot", 3, Spot.maxBucketSize());
+    assertEquals("maxBucketSize: Spot", 3, Spot.maxBucketSize(), 2);
 
     WordFrequency Frankenstein;
     Frankenstein.readIn("Frankenstein or The Modern Prometheus by Mary Shelley.txt");
@@ -40,7 +43,7 @@ int main() {
     assertEquals("wordCount: feared", 13, Frankenstein.wordCount("feared"));
     assertEquals("wordCount: the", 3903, Frankenstein.wordCount("the"));
     assertEquals("mostFrequentWord: Frankenstein", "the", Frankenstein.mostFrequentWord());
-    assertEquals("maxBucketSize: Frankenstein", 7, Frankenstein.maxBucketSize());
+    assertEquals("maxBucketSize: Frankenstein", 7, Frankenstein.maxBucketSize(), 1);
 
     WordFrequency SleepyHollow;
     SleepyHollow.readIn("The Legend of Sleep Hollow by Washington Irving.txt");
@@ -48,7 +51,7 @@ int main() {
     assertEquals("wordCount: Ichabod", 36, SleepyHollow.wordCount("Ichabod"));
     assertEquals("wordCount: the", 865, SleepyHollow.wordCount("the"));
     assertEquals("mostFrequentWord: Sleep Hollow", "the", SleepyHollow.mostFrequentWord());
-    assertEquals("maxBucketSize: Sleep Hollow", 6, SleepyHollow.maxBucketSize());
+    assertEquals("maxBucketSize: Sleep Hollow", 6, SleepyHollow.maxBucketSize(), 1);
 
     return 0;
 }
@@ -88,6 +91,23 @@ bool assertEquals(const string& nameOfTest, const T& expected, const U& actual) 
         // Green colored text
         cout << GREEN << "PASSED "
              << RESET << nameOfTest << ": expected and actual '" << actual << "'" << endl;
+        testCount++;
+        return true;
+    }
+    // Red colored text
+    cout << RED << "FAILED "
+         << RESET << nameOfTest << ": expected '" << expected << "' but actually '" << actual << "'" << endl;
+    return false;
+}
+
+template <typename T, typename U>
+bool assertEquals(const string& nameOfTest, const T& expected, const U& actual, unsigned delta) {
+    T minValue = (expected < delta)? 0: (expected-delta);
+    T maxValue = expected+delta;
+    if ((actual >= minValue) && (actual <= maxValue)) {
+        // Green colored text
+        cout << GREEN << "PASSED "
+          << RESET << nameOfTest << ": expected '" << expected << "' close to actual '" << actual << "'" << endl;
         testCount++;
         return true;
     }
